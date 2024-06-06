beba976d217586d7d918219b206e4382ec12e0a7
ManishD23 <manishdwivedi0327@gmail.com>
2024-06-06 23:34:08 +0530

    build html files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import { resolve } from "path";
 export default {
   root: resolve(__dirname, "src"),
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src", "index.html"),
+        about: resolve(__dirname, "src", "about.html"),
+        contact: resolve(__dirname, "src", "contact-us.html"),
+        project: resolve(__dirname, "src", "projects.html"),
+      },
+    },
     outDir: "../dist",
   },
   server: {
